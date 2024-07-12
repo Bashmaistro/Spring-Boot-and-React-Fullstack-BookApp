5b53ce6aa2aaeabf9b58eeba4e4977077a2acf5f
Bashmaistro <emirhanyildiz@ogr.eskisehir.edu.tr>
2024-07-12 11:13:18 +0300

    Carousel Component

--- FrontEnd/library-app/src/App.tsx
+++ FrontEnd/library-app/src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css';
+import { Carousel } from './layouts/HomePage/Carousel';
 import { ExploreTopbooks } from './layouts/HomePage/ExploreTopBooks';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
@@ -8,6 +9,7 @@ function App() {
   <div>
     <Navbar/>
     <ExploreTopbooks/>
+    <Carousel/>
 
   </div>
     
